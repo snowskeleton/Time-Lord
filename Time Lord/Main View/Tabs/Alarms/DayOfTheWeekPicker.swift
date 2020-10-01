cbaf14a063e12a67654e5255820be1166fa767cf
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-10-01 12:49:48 -0400

    MVP for the day selection screen.

--- Time Lord/Main View/Tabs/Alarms/DayOfTheWeekPicker.swift
+++ Time Lord/Main View/Tabs/Alarms/DayOfTheWeekPicker.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct DayOfTheWeekPicker: View {
+    //    @Binding var alarm: Alarm
+    @State private var activeDays = [false, false, false, false, false, false, false]
+    @State private var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    //    @State private var
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(days, id: \.self) { day in
+                Button(action: {
+                    activeDays[days.firstIndex(of: day)!].toggle()
+                }) {
+                    HStack {
+                        Text("\(day)")
+                        if activeDays[days.firstIndex(of: day)!] == true {
+                            Spacer()
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+            }
+            Button(action: {
+                for i in 1...5 {
+                    activeDays[i] = true
+                    //eventually I'll need to make this toggle based on some criteria
+                }
+            }) {
+                Text("Weekdays")
+            }
+            Button(action: {
+                activeDays[0] = true
+                activeDays[6] = true
+                //same here
+            }) {
+                Text("Weekends")
+            }
+        }
     }
 }
 
-struct DayOfTheWeekPicker_Previews: PreviewProvider {
-    static var previews: some View {
-        DayOfTheWeekPicker()
-    }
-}
