aeb66805e45a58beb1bf070e1531a4c6ec5f3cb9
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-10-11 18:12:18 -0400

    Manually managing this class.

---
+++ Alarm+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Alarm+CoreDataClass.swift
+//  Time Lord
+//
+//  Created by Isaac Lyons on 10/11/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Alarm)
+public class Alarm: NSManagedObject {
+
+}
