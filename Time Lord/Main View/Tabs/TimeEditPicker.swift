756bfc319ea3c659b9ddadafb42b2e3fa9938794
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-10-11 15:36:44 -0400

    Split off the wheel picker into its own separate view. Cleans things up a bit.

---
+++ Time Lord/Main View/Tabs/TimeEditPicker.swift
@@ -0,0 +1,20 @@
+//
+//  TimeEditPicker.swift
+//  Time Lord
+//
+//  Created by Isaac Lyons on 10/11/20.
+//
+
+import SwiftUI
+
+struct TimeEditPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimeEditPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeEditPicker()
+    }
+}
