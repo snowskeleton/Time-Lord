cb3464d9c8981d7c1cdba6d425a2cb29ce2420a3
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-10-11 15:37:18 -0400

    Flushed it out. Need to do something about am/pm selector in the future.

--- Time Lord/Main View/Tabs/TimeEditPicker.swift
+++ Time Lord/Main View/Tabs/TimeEditPicker.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct TimeEditPicker: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @Binding var selectedHour: Int
+    @Binding var selectedMinute: Int
+    @State private var amOrPm = [0, 1]
 
-struct TimeEditPicker_Previews: PreviewProvider {
-    static var previews: some View {
-        TimeEditPicker()
+    var body: some View {
+        GeometryReader { geometry in
+            HStack(spacing: 0) {
+                Spacer()
+                Picker("", selection: self.$selectedHour) {
+                    ForEach(0..<24) {
+                        Text(String($0)).tag($0)
+                    }
+                }
+                .labelsHidden()
+                .fixedSize(horizontal: true, vertical: true)
+                .frame(width: geometry.size.width / 5, height: 160)
+                .clipped()
+
+                Picker("", selection: self.$selectedMinute) {
+                    ForEach(0..<60) {
+                        Text(String($0)).tag($0)
+                    }
+                }
+                .labelsHidden()
+                .fixedSize(horizontal: true, vertical: true)
+                .frame(width: geometry.size.width / 5, height: 160)
+                .clipped()
+
+                Picker("", selection: self.$amOrPm) {
+                    Text("AM").tag(0)
+                    Text("PM").tag(1)
+                }
+                .labelsHidden()
+                .fixedSize(horizontal: true, vertical: true)
+                .frame(width: geometry.size.width / 5, height: 160)
+                .clipped()
+
+                Spacer()
+            }
+        }
+        .frame(height: 160)
+        .mask(Rectangle())
     }
 }
