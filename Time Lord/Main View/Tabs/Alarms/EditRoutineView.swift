6d4835ad78415350597d83c0ccb9296edf78b226
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-10-18 17:04:35 -0400

    Added file.

---
+++ Time Lord/Main View/Tabs/Alarms/EditRoutineView.swift
@@ -0,0 +1,20 @@
+//
+//  EditRoutineView.swift
+//  Time Lord
+//
+//  Created by Isaac Lyons on 10/18/20.
+//
+
+import SwiftUI
+
+struct EditRoutineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditRoutineView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditRoutineView()
+    }
+}
