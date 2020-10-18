0b30ebfd29ad3a6c40390472d1a286c3c6cdccd5
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-10-18 16:48:52 -0400

    Added file.

---
+++ Time Lord/Main View/Tabs/Alarms/RoutinePicker.swift
@@ -0,0 +1,20 @@
+//
+//  RoutinePickerr.swift
+//  Time Lord
+//
+//  Created by Isaac Lyons on 10/18/20.
+//
+
+import SwiftUI
+
+struct RoutinePickerr: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoutinePickerr_Previews: PreviewProvider {
+    static var previews: some View {
+        RoutinePickerr()
+    }
+}
