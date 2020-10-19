8280fa9073e70f0ee05b1ccb5f1327a97e17e4e6
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-10-19 16:11:53 -0400

    Added file.

---
+++ Time Lord/Main View/Tabs/Alarms/RoutinesView.swift
@@ -0,0 +1,20 @@
+//
+//  RoutinesView.swift
+//  Time Lord
+//
+//  Created by Isaac Lyons on 10/19/20.
+//
+
+import SwiftUI
+
+struct RoutinesView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RoutinesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoutinesView()
+    }
+}
